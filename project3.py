3718e3bc67350b9ac04c7d8aa0481709e58b8180
xanthosgeorge <56501711+xanthosgeorge@users.noreply.github.com>
2020-02-01 23:27:30 +0200

    Create project3.py

---
+++ project3.py
@@ -0,0 +1,18 @@
+listapragmatwn = {}
+#Εισαγωγή αντικειμένων στο Dictionary 
+while True: 
+    listapragmatwn[input("Δώστε το αντικείμενο που θέλετε να βάλετε στη λίστα \n")] =input("Δώστε τιμή του αντικείμενου \n")
+    x=input("Επιθυμήτε να σταματήσετε τη διαδικασία πρόσθεσης ?? \n")
+    if (x=="yes"):
+        break
+#Δημιουργεία λίστας με τις τιμές του Dictionary
+listapragmatwn_list=list(listapragmatwn.values())
+print(listapragmatwn_list)
+s=0
+#Υπολογσιμός του συνολικού κόστους 
+for i  in range(0,len(listapragmatwn_list)):
+    s=s+float(listapragmatwn_list[i])
+kostos = s *1.24
+print(kostos)
+
+
