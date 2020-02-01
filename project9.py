b6efb2215fce4aba90cb0354ce023c17f039bdab
xanthosgeorge <56501711+xanthosgeorge@users.noreply.github.com>
2020-02-01 23:27:41 +0200

    Create project9.py

---
+++ project9.py
@@ -0,0 +1,25 @@
+while True:
+    x =input("Δώστε ενα εριθμό που να μήν είναι μονοψήφιος")
+    if int(x)%10!=int(x):
+        break
+adder=int(x)
+limit = len(x)
+sum=0
+divider = int(x)
+for x in range (0,limit+1):
+    sum = sum +divider%10
+    divider = divider//10
+sum2 = 0
+while True:
+    sum2 = sum2 + adder*3+1+sum
+    adder= adder//10
+    if adder%10==adder:
+        break 
+print(sum2)   
+
+
+    
+   
+     
+
+    
